05667c1120f6ce9419da040372df84318d3465b9
Gustavo Biazini <234491887+gustavobiazini@users.noreply.github.com>
2026-05-20 21:26:07 +0100

    Form cCompras

--- Projeto DA/WindowsFormsApp1/WindowsFormsApp1/Views/ModoCompra.Designer.cs
+++ Projeto DA/WindowsFormsApp1/WindowsFormsApp1/Views/ModoCompra.Designer.cs
@@ -30,11 +30,17 @@
         {
             this.labelmodocompra = new System.Windows.Forms.Label();
             this.panelModoCompra = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Artigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.QtdPrevista = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.QtdAdquirida = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PrecoUnit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Subtotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panelModoCompra.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -43,7 +49,7 @@
             // 
             this.labelmodocompra.AutoSize = true;
             this.labelmodocompra.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelmodocompra.Location = new System.Drawing.Point(23, 27);
+            this.labelmodocompra.Location = new System.Drawing.Point(23, 38);
             this.labelmodocompra.Name = "labelmodocompra";
             this.labelmodocompra.Size = new System.Drawing.Size(249, 25);
             this.labelmodocompra.TabIndex = 0;
@@ -56,21 +62,28 @@
             this.panelModoCompra.Controls.Add(this.textBox1);
             this.panelModoCompra.Controls.Add(this.label2);
             this.panelModoCompra.Controls.Add(this.label1);
-            this.panelModoCompra.Location = new System.Drawing.Point(450, 77);
+            this.panelModoCompra.Location = new System.Drawing.Point(941, 77);
             this.panelModoCompra.Name = "panelModoCompra";
             this.panelModoCompra.Size = new System.Drawing.Size(666, 510);
             this.panelModoCompra.TabIndex = 1;
             // 
-            // label1
+            // dateTimePicker1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(13, 7);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(145, 25);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Orçamento N.º: ";
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.Color.White;
+            this.dateTimePicker1.Location = new System.Drawing.Point(463, 14);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(165, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 15);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Valor Do orc";
             // 
             // label2
             // 
@@ -83,42 +96,79 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Valor Disponível: ";
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(165, 14);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 15);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.Text = "Valor Do orc";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(13, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Orçamento N.º: ";
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Artigo,
+            this.Tipo,
+            this.QtdPrevista,
+            this.QtdAdquirida,
+            this.PrecoUnit,
+            this.Subtotal});
             this.dataGridView1.Location = new System.Drawing.Point(28, 77);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(401, 510);
+            this.dataGridView1.Size = new System.Drawing.Size(795, 510);
             this.dataGridView1.TabIndex = 2;
             // 
-            // dateTimePicker1
+            // Artigo
             // 
-            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.Color.White;
-            this.dateTimePicker1.Location = new System.Drawing.Point(463, 14);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
-            this.dateTimePicker1.TabIndex = 6;
+            this.Artigo.HeaderText = "Artigo";
+            this.Artigo.MinimumWidth = 6;
+            this.Artigo.Name = "Artigo";
+            // 
+            // Tipo
+            // 
+            this.Tipo.HeaderText = "Tipo";
+            this.Tipo.MinimumWidth = 6;
+            this.Tipo.Name = "Tipo";
+            // 
+            // QtdPrevista
+            // 
+            this.QtdPrevista.HeaderText = "Qtd. Prevista";
+            this.QtdPrevista.MinimumWidth = 6;
+            this.QtdPrevista.Name = "QtdPrevista";
+            // 
+            // QtdAdquirida
+            // 
+            this.QtdAdquirida.HeaderText = "Qtd. Adquirida";
+            this.QtdAdquirida.MinimumWidth = 6;
+            this.QtdAdquirida.Name = "QtdAdquirida";
+            // 
+            // PrecoUnit
+            // 
+            this.PrecoUnit.HeaderText = "Prreço Unit.";
+            this.PrecoUnit.MinimumWidth = 6;
+            this.PrecoUnit.Name = "PrecoUnit";
+            // 
+            // Subtotal
+            // 
+            this.Subtotal.HeaderText = "Subtotal";
+            this.Subtotal.MinimumWidth = 6;
+            this.Subtotal.Name = "Subtotal";
             // 
             // ModoCompra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1153, 599);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(1619, 700);
             this.Controls.Add(this.panelModoCompra);
             this.Controls.Add(this.labelmodocompra);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "ModoCompra";
             this.Text = "Modo Compra";
             this.panelModoCompra.ResumeLayout(false);
@@ -138,5 +188,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Artigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn QtdPrevista;
+        private System.Windows.Forms.DataGridViewTextBoxColumn QtdAdquirida;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PrecoUnit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Subtotal;
     }
 }

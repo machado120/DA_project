46b581a548b0a646e1096d4510348bad76ad34e3
Gustavo Biazini <234491887+gustavobiazini@users.noreply.github.com>
2026-06-09 22:32:13 +0100

    Alterie BD

--- Projeto DA/WindowsFormsApp1/WindowsFormsApp1/Models/AppDbInitializer.cs
+++ Projeto DA/WindowsFormsApp1/WindowsFormsApp1/Models/AppDbInitializer.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using WindowsFormsApp1.Models;
 
 
 namespace WindowsFormsApp1.Models
@@ -14,9 +17,14 @@ namespace WindowsFormsApp1.Models
     {
         protected override void Seed(Context context)
         {
-            // Alterar para o vosso context
+            context.Utilizadores.AddOrUpdate(
+                u => u.Username,
+                new Utilizadores { Username = "admin", Password = "admin" }
+            );
 
             base.Seed(context);
         }
+        
     }
+
 }
